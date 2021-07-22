6f16b681ad1b11a5b3467f08185fec5b9cac357f
smooooky <75350537+smooooky@users.noreply.github.com>
2021-07-21 22:47:15 -0400

    fixed combine media-query plugin, and removed unecessary node modules

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,7 @@ const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
 const autoprefixer = require('gulp-autoprefixer');
 const postcss = require('gulp-postcss');
-const fs = require("fs");
-const mqpacker = require("@hail2u/css-mqpacker");
+const combineMediaQuery = require('postcss-combine-media-query');
 
 browserSync.init({
     injectChanges: true,
@@ -14,27 +13,20 @@ browserSync.init({
 function compileSass() {
  return src('scss/main.scss')
  .pipe(sass())
+ .pipe(postcss([
+  combineMediaQuery()
+  ]))
  .pipe(autoprefixer())
  .pipe(dest('css'))
  .pipe(browserSync.stream());
 }
 
 function css() {
-  // return src('css/main.css')
-  //   .pipe(postcss([
-  //     require("@hail2u/css-mqpacker")()
-  //   ]).process(fs.readFileSync("css/main.css", "utf8")).then(function (result) {
-  //     console.log(result.css);
-  //   }))
-  //   .pipe(dest('css'));
   return src('css/main.css')
-  .pipe(postcss([mqpacker.pack(fs.readFileSync("css/main.css", "utf8"), {
-    from: "css/main.css",
-    map: {
-      inline: false
-    },
-    to: "css/main.css"
-  }).css]));
+  .pipe(postcss([
+    combineMediaQuery()
+    ]))
+  .pipe(dest('css'))
 }
 
 function watchHtml() {
@@ -45,6 +37,6 @@ function watchHtml() {
 exports.default = function(){
     //compile and watch
     watch('scss/*.scss',compileSass);
-    watch('css/main.css',css);
+    // watch('css/main.css',css);
     watch('index.html',watchHtml);
 };
