048645c54465b26695e24797c712fc8619d3f864
smooooky <75350537+smooooky@users.noreply.github.com>
2022-04-01 01:38:31 -0400

    added hamburger menu functionality/styles/aria attr

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 (function(){
+    let btn = document.getElementById('hamburger_menu');
 
     function sticky () {
         let bar = document.getElementById('sticky_nav');
@@ -9,5 +10,19 @@
             bar.classList.remove('active');
         }
     }
+
+    function toggleMenu() {
+        
+        let menu =  document.getElementById('main_menu');
+
+        if(!menu.classList.contains('active')){
+            menu.classList.add('active');
+            btn.setAttribute('aria-expanded','true');
+        } else {
+            menu.classList.remove('active');
+            btn.setAttribute('aria-expanded','false');
+        }
+    }
+    btn.addEventListener('click',toggleMenu);
     window.addEventListener('scroll',sticky);
 }());
